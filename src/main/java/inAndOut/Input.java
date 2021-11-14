7ad256a20774686009f9cfd4c50ded35cf417739
skarltjr <kisa0828@naver.com>
2021-11-15 00:48:25 +0900

    feat(Input) : add Input to get informations from user

--- src/main/java/inAndOut/Input.java
+++ src/main/java/inAndOut/Input.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 
 public class Input {
-    public static String ERROR = "[ERROR]";
     private Scanner scanner;
     private List<String> carNames;
     private int count;
@@ -15,58 +14,32 @@ public class Input {
         this.scanner = scanner;
         this.carNames = new ArrayList<>();
         this.count = 0;
+        this.validator = new IOValidator();
     }
 
-    public void getInput() {
+    public void setInput() {
         System.out.println("입력해주세요");
         //validation
-        String names = validateCarNames();
+        String names = validator.validateCarNames(scanner.nextLine());
         // 자동차 이름 get
-        getCarNames(names);
+        addCarNames(names);
         // 시도 횟수 validation get
         System.out.println("회수 입력");
-        validateCount();
-
+        this.count = validator.validateCount(scanner.nextLine());
     }
 
-    private void validateCount() {
-        String countVal = scanner.nextLine();
-        checkIfEmpty(countVal);
-        for (int i = 0; i < countVal.length(); i++) {
-            if (!Character.isDigit(countVal.charAt(i))) {
-                throw new RuntimeException(ERROR + " 시도 횟수는 숫자여야합니다.");
-            }
-        }
-        this.count = Integer.parseInt(countVal);
-        System.out.println(count);
-    }
-
-    private void checkIfEmpty(String input) {
-        if (input.isEmpty()) {
-            throw new RuntimeException(ERROR + " 입력을 확인해주세요.");
-        }
-    }
-
-    private String validateCarNames() {
-        String line = scanner.nextLine();
-        checkIfEmpty(line);
-        checkInputContainsComma(line);
-        return line;
-
-    }
-
-    private void checkInputContainsComma(String input) {
-        if (!input.contains(",")) {
-            throw new RuntimeException(ERROR + " ,을 활용하여 구분해주세요");
-        }
-    }
-
-    private void getCarNames(String names) {
+    private void addCarNames(String names) {
         String[] split = names.split(",");
         for (String name : split) {
             carNames.add(name);
         }
     }
 
+    public List<String> getCarNames() {
+        return this.carNames;
+    }
 
+    public int getCount() {
+        return this.count;
+    }
 }
